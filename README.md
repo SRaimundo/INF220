cb7227c0dd1113b064bb561c8b889272950639ed
SRaimundo <samuel.r.pinto@ufv.br>
2022-12-06 01:15:44 -0300

    atualizando README

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Trabalho de INF220
 
 Para instalar os modulos basta clonar o repositorio e colocar npm install (tem que ter o npm e node instalados na maquina)"
+Esqueci como fazer para deixar as senhas ocultas, então cada coloca as informações da sua conexao em "db.ts"
--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
 import {createPool} from "mysql2/promise";
 
 
-// export const connect = async () => {
-//     const connection = await createPool({
-//         host: ,
-//         user: ,
-//         database: ,
-//         password: 
-//     });
-//     return connection;
-// }
+export const connect = async () => {
+    const connection = await createPool({
+        host: 'localhost',
+        user: 'root',
+        database: 'BOM_SONO',
+        password: ''
+    });
+    return connection;
+}
