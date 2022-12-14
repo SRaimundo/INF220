e09b3b545ae732368f8b68f616107d23b70833f5
joaosolanogesuas <joaolucas.solano@gesuas.com.br>
2022-12-14 17:16:43 -0300

    [TIPO APARTAMENTO] Rotas tipo de apartamento atualizada

--- back/src/interfaces/roomTypes.ts
+++ back/src/interfaces/roomTypes.ts
@@ -1,8 +1,8 @@
 export interface RoomType{
     Id_tipo: number;
-    Valor:number;
-    Tem_TV:boolean;
-    Adaptado:boolean;
-    Numero_camas_solteiro: number;
+    Valor: number;
+    Tem_TV: boolean;
+    Adaptado: boolean;
     Numero_camas_casal: number;
+    Numero_camas_solteiro: number;
 }
