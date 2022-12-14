575cffc652220828c22363cbcf41466ba85f2841
joaosolanogesuas <joaolucas.solano@gesuas.com.br>
2022-12-14 13:38:03 -0300

    [Arrumação] Rotas de arrumação atualizadas

--- back/src/models/housekeeping.ts
+++ back/src/models/housekeeping.ts
@@ -4,10 +4,33 @@ import { connect } from "../db";
 class Housekeeping{
     static getAll = async () => {
         const conn = await connect();
-        const q = "SELECT A.Data, F.Nome, H.Nome as Hotel, A.Quarto FROM ARRUMACAO A, FUNCIONARIO F, HOTEL H, QUARTO Q WHERE A.Funcionario = F.idFuncionario AND A.Quarto = Q.Numero AND H.idHotel = Q.Hotel";
+        const q = "SELECT * FROM ARRUMACAO";
         const linhas = await conn.query(q);
         return linhas[0];
     };
+
+    static findOne = async (quarto: any, hotel: any, data: any, funcionario: any) => {
+        const conn = await connect();
+        const q = "SELECT * FROM ARRUMACAO WHERE Quarto = ? AND Hotel = ? AND Data = date(?) AND Funcionario = ?";
+        const linhas = await conn.query(q, [quarto, hotel, data, funcionario]);
+        return linhas[0];
+    };
+
+    static delete = async (quarto: any, hotel: any, data: any, funcionario: any) => {
+        const conn = await connect();
+        const q = "DELETE FROM ARRUMACAO WHERE Quarto = ? AND Hotel = ? AND Data = date(?) AND Funcionario = ?";
+        const linhas = await conn.query(q, [quarto, hotel, data, funcionario]);
+        return linhas[0];
+    };
+
+    static create = async (houseKeeping: HousekeepingInterface) => {
+        const conn = await connect();
+        const [rows, fields] = await conn.query(
+            `INSERT INTO ARRUMACAO (Quarto, Hotel, Data, Funcionario) VALUES (?, ?, ?, ?)`,
+            [houseKeeping.Quarto, houseKeeping.Hotel, houseKeeping.Data, houseKeeping.Funcionario]
+        );
+        return rows;
+    }
 }
 
 export default Housekeeping;
--- back/src/routes/housekeeping.ts
+++ back/src/routes/housekeeping.ts
@@ -3,10 +3,10 @@ import controller from "../controllers/housekeeping";
 
 const router = express.Router();
 
-router.post("/",);
+router.post("/",controller.create);
 router.get("/",controller.getAll);
-router.patch("/");
-router.delete("/");
+router.get("/:quarto/:hotel/:data/:funcionario",controller.findOne);
+router.delete("/:quarto/:hotel/:data/:funcionario",controller.deleteOne);
 
 
 export default router;
